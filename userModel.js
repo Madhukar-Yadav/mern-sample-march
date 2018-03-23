1d6cfd7f47f4c1be1c98754c22751fa7222fee07
Madhukar Yadav B <35990808+Madhukar-Yadav@users.noreply.github.com>
2018-03-23 18:33:32 +0530

    Add files via upload

---
+++ userModel.js
@@ -0,0 +1,7 @@
+var User = [
+    { id:'1', username: 'Admin', password: 'Admin123', gender: 'male', country: 'India' },
+    { id:'2', username: 'Madhu', password: 'Madhu123', gender: 'male', country: 'India' }
+]
+
+
+module.exports = User;
